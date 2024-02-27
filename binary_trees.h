5ed5516aba5dc7861848647bb9fa79c73c647479
marky376 <mmutuamark@gmail.com>
2024-02-27 07:04:22 +0300

    A commit on files to work on

--- binary_trees.h
+++ binary_trees.h
@@ -19,4 +19,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
 #endif
