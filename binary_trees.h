97b22762928847e75d587e29d29d7ed046e95eb1
marky376 <mmutuamark@gmail.com>
2024-02-27 06:30:02 +0300

    A commit on files to work on

--- binary_trees.h
+++ binary_trees.h
@@ -13,6 +13,7 @@ void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif
