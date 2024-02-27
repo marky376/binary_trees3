51f16dbffaa89afb017161b3c44fe474208837a4
marky376 <mmutuamark@gmail.com>
2024-02-27 06:28:08 +0300

    A commit on files to work on

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREEE_H
 #define BINARY_TREE_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
