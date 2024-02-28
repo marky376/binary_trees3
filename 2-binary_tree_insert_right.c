41f03e23064f3056a4c973dd6025ae28d9971600
marky376 <mmutuamark@gmail.com>
2024-02-28 09:40:22 +0300

    The binary tree project

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     if (parent == NULL)
         return (NULL);
 
+    new_node = malloc(sizeof(binary_tree_t));
     new_node = binary_tree_node(parent, value);
     if (new_node == NULL)
         return (NULL);
