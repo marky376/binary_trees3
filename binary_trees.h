1bdbf27e3da0ef8f448ea39883c855f007e2d8b4
marky376 <mmutuamark@gmail.com>
2024-02-28 09:11:36 +0300

    The binary tree project

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
