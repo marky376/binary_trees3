65fbc462f1ceeae413064f7e705c5421461ab0a2
marky376 <mmutuamark@gmail.com>
2024-02-28 09:07:43 +0300

    The binary tree project

--- binary_trees.h
+++ binary_trees.h
@@ -46,4 +46,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
 #endif
