03831ee20d0f9bd04cbb2e392d83b10b496fedc3
marky376 <mmutuamark@gmail.com>
2024-02-28 10:37:10 +0300

    The binary tree project

--- binary_trees.h
+++ binary_trees.h
@@ -52,5 +52,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 //static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 //static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
-//int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node);
 #endif
